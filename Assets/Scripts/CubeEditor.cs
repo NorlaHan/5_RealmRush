4dd96ab639406112a928c8f33c3a9a7ac7d5c618
PhhP <ph_430@hotmail.com>
2018-08-27 11:11:44 +0800

    A cube will snap in the editor and display its coresponding coordinate.

---
+++ Assets/Scripts/CubeEditor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CubeEditor : MonoBehaviour {
+
+	[SerializeField][Range(5,15)] int gridSize = 10;
+
+	TextMesh textMesh;
+	// Use this for initialization
+	void Awake() 
+	{
+		print(name + ", awake");
+	}
+	void Start () {
+		print(name + ", start");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		print(name + ", updated");
+		Vector3 snapPos;		
+
+		snapPos.x = Mathf.RoundToInt(transform.position.x/gridSize);
+		snapPos.y = Mathf.RoundToInt(transform.position.y/gridSize);
+		snapPos.z = Mathf.RoundToInt(transform.position.z/gridSize);
+		transform.position = snapPos*gridSize;
+
+		textMesh = GetComponentInChildren<TextMesh>();
+		textMesh.text =snapPos.x.ToString() +" , "+snapPos.z.ToString();
+	}
+}
